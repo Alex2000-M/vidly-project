1b7b619da8dc4a905835b087ce276f7f2b132885
Amir M <amsadeghi79@gmail.com>
2023-04-24 22:19:24 +0330

    5th commit

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const winston = require('winston');
 const express = require('express');
 const app = express();
+const redis = require('redis');
+const client = redis.createClient();
+const { Genre } = require('./models/genre');
 
+(async () => {
+    await client.connect();
+})();
+
+app.get('/', async (req, res) => {
+    const value = await client.get('gen');
+    if (value !== null) {
+        console.log('Cache Hit!')
+        return res.send(JSON.parse(value))
+    }
+    else {
+        const genres = await Genre.find();
+        await client.setEx('gen', 3600, JSON.stringify(genres))
+        console.log('Cache Miss!')
+        return res.send(genres)
+    }
+})
 
 require('./startup/db')();
 require('./startup/routes')(app);
 require('./startup/validation')();
 require('./startup/logging')();
+
+
 
 
 
--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const winston = require('winston');
+require('dotenv').config();
 
 module.exports = function() {
+    require('dotenv').config();
     const db = process.env.MONGO_URL;
     mongoose.connect(db)
         .then(() => winston.info('Connected to MongoDB...'))
